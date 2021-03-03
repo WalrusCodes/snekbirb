dcfd3bdec720085d042c6cf9ca44b9b5b2830f51
WalrusWalrus <walruscodes@users.noreply.github.com>
2021-03-03 13:28:53 -0800

    take level name from command line

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,12 @@ use std::path::Path;
 use snekbirb;
 
 fn main() {
-    let path = Path::new("levels/4.txt");
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 2 {
+        println!("Usage: snekbirb <level>");
+        return;
+    }
+    let path = Path::new(&args[1]);
     let maybe_answer = snekbirb::solve(&path);
     if let Some(answer) = maybe_answer {
         println!("winning moves: {}", answer);
